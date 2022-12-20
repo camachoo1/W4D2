89b2baa7a910d65d418a60caa55d7708303ce5be
“Omar <ocamacho812@gmail.com>
2022-12-20 15:23:59 -0800

    adding methods to piece class

--- chess/pieces/piece.rb
+++ chess/pieces/piece.rb
@@ -8,4 +8,23 @@ class Piece
     @pos = pos
   end
   
+  def to_s
+    " #{symbol}"
+  end
+
+  def empty?
+    false
+  end
+
+  def valid_moves?
+
+  end
+
+  def pos=(val)
+  end
+
+
+  private
+  def move_into_check?(end_pos)
+  end
 end
