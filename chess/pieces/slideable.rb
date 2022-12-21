f98dc6bf2d0f3a48e943e5eb0e92bede534df39b
“Omar <ocamacho812@gmail.com>
2022-12-20 23:34:15 -0800

    Swap methods in slideable and steppable - mistake

--- chess/pieces/slideable.rb
+++ chess/pieces/slideable.rb
@@ -1,7 +1,34 @@
 module Slideable
-  
+  HORIZONTAL_DIRS = [[0, -1], [0, 1], [1, 0], [-1, 0]]
+  DIAGONAL_DIRS = [[-1, 1], [-1, -1], [1, 1], [1, -1]]
 
+  HORIZONTAL_AND_DIAGONAL_DIRS = HORIZONTAL_DIRS + DIAGONAL_DIRS
+  
   def moves
-    
+    moves = []
+    move_dirs.each { |dx, dy| moves + grow_unblocked_moves_in_dir(dx, dy) }
+    moves
   end
+
+  private
+  def grow_unblocked_moves_in_dir(dx, dy)
+    x, y = pos
+    moves = []
+    while
+      x, y = x + dx, y + dy
+      pos = [x, y]
+
+      break unless board.valid_pos?(pos)
+
+      if board.empty?(pos)
+        moves << pos
+      else
+        moves << pos if board[pos].color != color
+        break
+      end
+    end
+    moves
+  end
+
+  
 end
